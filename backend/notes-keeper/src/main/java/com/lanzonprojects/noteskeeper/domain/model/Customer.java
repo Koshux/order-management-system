7a464b14afab1a200f3f89a6ed7ae78a0f654c1d
Koshux <lanzonprojects@gmail.com>
2019-10-27 16:07:30 +0100

    Added get/set on Customer
    1. Added getters/setter methods as well as the toString method for the Customer JSON:API resource.

--- backend/notes-keeper/src/main/java/com/lanzonprojects/noteskeeper/domain/model/Customer.java
+++ backend/notes-keeper/src/main/java/com/lanzonprojects/noteskeeper/domain/model/Customer.java
@@ -35,4 +35,164 @@ public class Customer {
     private List<String> behaviouralProblems;
     private List<String> blacklistIngredients;
     private List<String> toysTypes;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Timestamp getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Timestamp creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getHouseName() {
+        return houseName;
+    }
+
+    public void setHouseName(String houseName) {
+        this.houseName = houseName;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public int getDogsCount() {
+        return dogsCount;
+    }
+
+    public void setDogsCount(int dogsCount) {
+        this.dogsCount = dogsCount;
+    }
+
+    public List<String> getDogNames() {
+        return dogNames;
+    }
+
+    public void setDogNames(List<String> dogNames) {
+        this.dogNames = dogNames;
+    }
+
+    public int getLimitDogCalories() {
+        return limitDogCalories;
+    }
+
+    public void setLimitDogCalories(int limitDogCalories) {
+        this.limitDogCalories = limitDogCalories;
+    }
+
+    public int getDogAge() {
+        return dogAge;
+    }
+
+    public void setDogAge(int dogAge) {
+        this.dogAge = dogAge;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public List<String> getAllergies() {
+        return allergies;
+    }
+
+    public void setAllergies(List<String> allergies) {
+        this.allergies = allergies;
+    }
+
+    public List<String> getHealthIssues() {
+        return healthIssues;
+    }
+
+    public void setHealthIssues(List<String> healthIssues) {
+        this.healthIssues = healthIssues;
+    }
+
+    public List<String> getBehaviouralProblems() {
+        return behaviouralProblems;
+    }
+
+    public void setBehaviouralProblems(List<String> behaviouralProblems) {
+        this.behaviouralProblems = behaviouralProblems;
+    }
+
+    public List<String> getBlacklistIngredients() {
+        return blacklistIngredients;
+    }
+
+    public void setBlacklistIngredients(List<String> blacklistIngredients) {
+        this.blacklistIngredients = blacklistIngredients;
+    }
+
+    public List<String> getToysTypes() {
+        return toysTypes;
+    }
+
+    public void setToysTypes(List<String> toysTypes) {
+        this.toysTypes = toysTypes;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Customer{");
+        sb.append("id=").append(id);
+        sb.append(", creationDate=").append(creationDate);
+        sb.append(", customerName='").append(customerName).append('\'');
+        sb.append(", phoneNumber='").append(phoneNumber).append('\'');
+        sb.append(", houseName='").append(houseName).append('\'');
+        sb.append(", streetName='").append(streetName).append('\'');
+        sb.append(", postCode='").append(postCode).append('\'');
+        sb.append(", dogsCount=").append(dogsCount);
+        sb.append(", dogNames=").append(dogNames);
+        sb.append(", limitDogCalories=").append(limitDogCalories);
+        sb.append(", dogAge=").append(dogAge);
+        sb.append(", breed='").append(breed).append('\'');
+        sb.append(", allergies=").append(allergies);
+        sb.append(", healthIssues=").append(healthIssues);
+        sb.append(", behaviouralProblems=").append(behaviouralProblems);
+        sb.append(", blacklistIngredients=").append(blacklistIngredients);
+        sb.append(", toysTypes=").append(toysTypes);
+        sb.append('}');
+        return sb.toString();
+    }
 }
