4523f3d60128e84f73ec617f3a4b81359d1b5865
Koshux <lanzonprojects@gmail.com>
2019-11-24 18:39:30 +0100

    Added `home` link
    Added a link for the user to return back to home.  This however requires further UX enhancements.

--- frontend/order-management-system/src/components/app-bar.js
+++ frontend/order-management-system/src/components/app-bar.js
@@ -2,8 +2,10 @@
 import clsx from 'clsx'
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Route, MemoryRouter } from 'react-router'
-import { Link as RouterLink } from 'react-router-dom'
+import {
+  Link as RouterLink,
+  BrowserRouter as Router
+} from 'react-router-dom'
 
 // mine
 import Note from '../old_components/note'
@@ -12,9 +14,9 @@ import DataTable from '../old_components/notes-datatable'
 
 // mui-icons
 import MenuIcon from '@material-ui/icons/Menu'
+import HomeIcon from '@material-ui/icons/Home'
 import PeopleIcon from '@material-ui/icons/People'
 import ListAltIcon from '@material-ui/icons/ListAlt'
-import InboxIcon from '@material-ui/icons/MoveToInbox'
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
 import PermContactCalendarIcon from '@material-ui/icons/PermContactCalendar'
 
@@ -27,7 +29,6 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Divider from '@material-ui/core/Divider'
 import ListItem from '@material-ui/core/ListItem'
-import Container from '@material-ui/core/Container'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
 import CssBaseline from '@material-ui/core/CssBaseline'
@@ -97,7 +98,7 @@ function ListItemLink (props) {
 
   const renderLink = React.useMemo(
     () => React.forwardRef((itemProps, ref) => (
-        <RouterLink to={to} {...itemProps} innerRef={ref} />
+      <RouterLink to={to} {...itemProps} innerRef={ref} />
     )),
     [to]
   )
@@ -165,20 +166,21 @@ export default function LeftDrawerRouter(props) {
           paper: classes.drawerPaper,
         }}
       >
-        <div className={classes.drawerHeader}>
-          <IconButton onClick={handleDrawerClose}>
-            <ChevronLeftIcon />
-          </IconButton>
-        </div>
-        <Divider />
+        <Router initialEntries={['/']} initialIndex={0}>
+          <div className={classes.drawerHeader}>
+            <List aria-label="All pages for DogBox management system">
+              <ListItemLink to="/" primary="Home" icon={<HomeIcon />} />
+            </List>
 
-        <MemoryRouter initialEntries={['/drafts']} initialIndex={0}>
+            <IconButton onClick={handleDrawerClose}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </div>
+
+          <Divider />
+
           <div className={classes.root}>
-            <Route>
-              {({ location }) => (
-                <Typography gutterBottom>Current route: {location.pathname}</Typography>
-              )}
-            </Route>
+
             <Paper elevation={0}>
               <List aria-label="All pages for DogBox management system">
                 <ListItemLink to="/orders" primary="Orders" icon={<ListAltIcon />} />
@@ -187,63 +189,36 @@ export default function LeftDrawerRouter(props) {
               </List>
             </Paper>
           </div>
-        </MemoryRouter>
+        </Router>
       </Drawer>
       <main
         className={clsx(classes.content, {
           [classes.contentShift]: open,
         })}
       >
-        {/* <div className={classes.drawerHeader} /> */}
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-          donec massa sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+        <Grid container spacing={3}>
+          <Grid item xs={12}></Grid>
 
-        <Container fixed>
-          <Grid container spacing={3}>
-            <Grid item xs={12}></Grid>
-
-            <Grid item xs={10}>
-              <Note
-                note={data}
-                setNote={setData}
-              />
-            </Grid>
-
-            <Grid item xs={2}>
-              <Send
-                note={data}
-                materialTableRef={props.materialTableRef}
-              />
-            </Grid>
-
-            <Grid item xs={12}>
-              <DataTable
-                materialTableRef={props.materialTableRef}
-              />
-            </Grid>
+          <Grid item xs={10}>
+            <Note
+              note={data}
+              setNote={setData}
+            />
           </Grid>
-        </Container>
+
+          <Grid item xs={2}>
+            <Send
+              note={data}
+              materialTableRef={props.materialTableRef}
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <DataTable
+              materialTableRef={props.materialTableRef}
+            />
+          </Grid>
+        </Grid>
       </main>
     </div>
   )
